d47a006c2cec88e8d636f98171971036fccda5c7
AbdullahDurrani-0011 <abdullah.techtribe@gmail.com>
2025-07-29 21:55:21 +0500

    Tuesday 7/29/25

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const text1 = document.getElementById("InnerText1");
-const text2 = document.getElementById("InnerText2");
+// const text1 = document.getElementById("InnerText1");
+// const text2 = document.getElementById("InnerText2");
 
-InnerText1.addEventListener("input", () => {
-  InnerText2.value = InnerText1.value;
-});
+// InnerText1.addEventListener("input", () => {
+//   InnerText2.value = InnerText1.value;
+// });
 
-InnerText2.addEventListener("input", () => {
-  InnerText1.value = InnerText2.value;
-});
+// InnerText2.addEventListener("input", () => {
+//   InnerText1.value = InnerText2.value;
+// });
 
-const topArea = document.getElementById("InnerText1");
-const bottomArea = document.getElementById("InnerText2");
+// const topArea = document.getElementById("InnerText1");
+// const bottomArea = document.getElementById("InnerText2");
+
+// const topArea = document.getElementById("editor1");
+// const bottomArea = document.getElementById("editor2");
+
+
+
+// editor1.addEventListener("input", () => {
+//   editor2.value = editor1.value;
+// });
+
+// editor2.addEventListener("input", () => {
+//   editor1.value = editor2.value;
+// });
+
+
+
+
+
+
 
 const startingMinutes = 5;
 let time = startingMinutes * 60;
@@ -21,8 +40,10 @@ function updateCountdown() {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
 
-  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  countdownEl.innerText = `${minutes}: ${seconds}`;
   time--;
+
+  // console.log(time, "time---");
 
   if (time == 0) {
     if (topArea.disabled) {
@@ -36,5 +57,3 @@ function updateCountdown() {
     time = startingMinutes * 60;
   }
 }
-
-
